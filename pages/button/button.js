557aa5c76b3860ba86cd755e8f53f6aa3f53cce7
Michael Miao <maydaymiao@126.com>
2020-01-22 14:32:30 +0800

    1.22

--- pages/button/button.js
+++ pages/button/button.js
@@ -108,6 +108,13 @@ Page({
     .catch(err=>console.log(err))
   },
 
+  handleNavi(){
+    wx:wx.navigateTo({
+      url: '/pages/navi/navi'
+
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
